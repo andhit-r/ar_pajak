264605976ecb0eb8c57eae91480783ad63c659fb
Andhitia Rama <andhitia.r@gmail.com>
2013-08-16 00:21:35 +0700

    Gatekeeper guide

--- object_module/formulir_1111_b2.py
+++ object_module/formulir_1111_b2.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution   
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -28,125 +28,148 @@ from openerp import pooler
 from datetime import datetime
 
 class formulir_1111_b2(osv.osv):
-	_name = 'pajak.formulir_1111_b2'
-	_description = 'Formulir  1111 B2'
-	_inherit = ['mail.thread']
-	
-	def default_state(self, cr, uid, context={}):
-		return 'draft'
-		
-	def default_name(self, cr, uid, context={}):
-		return '/'
-		
-	def default_created_time(self, cr, uid, context={}):
-		#TODO: Ticket #5
-		return False
-		
-	def default_created_user_id(self, cr, uid, context={}):
-		#TODO: Ticket #6
-		return False
+    _name = 'pajak.formulir_1111_b2'
+    _description = 'Formulir  1111 B2'
+    _inherit = ['mail.thread']
+    
+    def default_state(self, cr, uid, context={}):
+        return 'draft'
+        
+    def default_name(self, cr, uid, context={}):
+        return '/'
+        
+    def default_created_time(self, cr, uid, context={}):
+        #TODO: Ticket #61
+        return False
+        
+    def default_created_user_id(self, cr, uid, context={}):
+        return uid
 
-	
-			
-	
-	_columns = 	{
-								'name' : fields.char(string='# SPT', size=30, required=True, readonly=True),
-								'company_id' : fields.many2one(string='Perusahaan', obj='res.company', required=True),
-								'nama_pkp' : fields.char(string='Nama PKP', size=255, required=True),
-								'npwp' : fields.char(string='NPWP', size=50, required=True),
-								'masa_pajak_id' : fields.many2one(string='Masa Pajak', obj='pajak.masa_pajak', required=True),
-								'pembetulan_ke' : fields.integer(string='Pembetulan Ke-', required=True),
-								'detail_ids' : fields.one2many(string='Detail', obj='pajak.detail_formulir_1111_b2', fields_id='formulir_id'),
-								'note' : fields.text(string='Note'),        
-								'state' : fields.selection([('draft','Draft'),('confirm','Waiting For Approval'),('approve','Ready To Process'),('done','Done'),('cancel','Cancel')], 'Status', readonly=True),
-								'created_time' : fields.datetime(string='Created Time', readonly=True),
-								'created_user_id' : fields.many2one(string='Created By', obj='res.users', readonly=True),
-								'confirmed_time' : fields.datetime(string='Confirmed Time', readonly=True),
-								'confirmed_user_id' : fields.many2one(string='Confirmed By', obj='res.users', readonly=True),						
-								'approved_time' : fields.datetime(string='Approved Time', readonly=True),
-								'approved_user_id' : fields.many2one(string='Approved By', obj='res.users', readonly=True),		
-								'processed_time' : fields.datetime(string='Processed Time', readonly=True),
-								'processed_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),				
-								'cancelled_time' : fields.datetime(string='Processed Time', readonly=True),
-								'cancelled_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),																								
-								'cancelled_reason' : fields.text(string='Cancelled Reason', readonly=True),
-								}	
-				
-	_defaults =	{
-							'name' : default_name,
-							'state' : default_state,
-							'created_time' : default_created_time,
-							'created_user_id' : default_created_user_id,
-							}
+    def function_amount_all(self, cr, uid, ids, name, args, context={}):
+        #TODO: Ticket #62
+        res = {}
+        for id in ids:
+            res[id] =   {
+                        'total_dpp' : 0.0,
+                        'total_ppn' : 0.0,
+                        'total_ppnbm' : 0.0,
+                        }
+        return res
 
-	def workflow_action_confirm(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'confirm'})
-		return True
-
-	def workflow_action_approve(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'approve'})
-		return True			
-		
-	def workflow_action_done(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'done'})
-		return True		
-		
-	def workflow_action_cancel(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'cancel'})
-		return True		
-		
-
-
-		
-	def button_action_set_to_draft(self, cr, uid, ids, context={}):
-		for id in ids:
-			if not self.delete_workflow_instance(self, cr, uid, id):
-				return False
-
-			if not self.create_workflow_instance(self, cr, uid, id):
-				return False
-				
-		return True
-
-		
-        def button_action_cancel(self, cr, uid, ids, context={}):
-                wkf_service = netsvc.LocalService('workflow')
-                for id in ids:
-                                if not self.delete_workflow_instance(self, cr, uid, id):
-                                        return False
-
-                                if not self.create_workflow_instance(self, cr, uid, id):
-                                        return False
-
-                                wkf_service.trg_validate(uid, 'pajak.formulir_1111_b2', id, 'button_cancel', cr)
-
-                return True
-
-        def log_audit_trail(self, cr, uid, id, event):
-                #TODO: Ticket #12
-                return True
-
-        def delete_workflow_instance(self, cr, uid, id):
-                #TODO: Ticket #13
-                return True
-
-        def create_workflow_instance(self, cr, uid, id):
-                #TODO: Ticket #14
-                return True
-                
-        def onchange_company_id(self, cr, uid, ids, comapny_id):
-            value = {}
-            domain = {}
-            warning = {}
+    
             
-            return {'value' : value, 'domain' : domain, 'warning' : warning}                   
+    
+    _columns =  {
+                                'name' : fields.char(string='# SPT', size=30, required=True, readonly=True),
+                                'company_id' : fields.many2one(string='Perusahaan', obj='res.company', required=True), #TODO: Ticket #67
+                                'nama_pkp' : fields.char(string='Nama PKP', size=255, required=True), #TODO: Ticket #67
+                                'npwp' : fields.char(string='NPWP', size=50, required=True), #TODO: Ticket #67
+                                'masa_pajak_id' : fields.many2one(string='Masa Pajak', obj='pajak.masa_pajak', required=True), #TODO: Ticket #67
+                                'pembetulan_ke' : fields.integer(string='Pembetulan Ke-', required=True), #TODO: Ticket #67
+                                'detail_ids' : fields.one2many(string='Detail', obj='pajak.detail_formulir_1111_b2', fields_id='formulir_id'), #TODO: Ticket #67
+                                'total_dpp' : fields.function(string='Total DPP', fnct=function_amount_all, digits_compute=dp.get_precision('Account'), method=True, store=True, multi='all'),
+                                'total_ppn' : fields.function(string='Total PPN', fnct=function_amount_all, digits_compute=dp.get_precision('Account'), method=True, store=True, multi='all'),
+                                'total_ppnbm' : fields.function(string='Total PPnBM', fnct=function_amount_all, digits_compute=dp.get_precision('Account'), method=True, store=True, multi='all'),
+                                'note' : fields.text(string='Note'),        
+                                'state' : fields.selection([('draft','Draft'),('confirm','Waiting For Approval'),('approve','Ready To Process'),('done','Done'),('cancel','Cancel')], 'Status', readonly=True),
+                                'created_time' : fields.datetime(string='Created Time', readonly=True),
+                                'created_user_id' : fields.many2one(string='Created By', obj='res.users', readonly=True),
+                                'confirmed_time' : fields.datetime(string='Confirmed Time', readonly=True),
+                                'confirmed_user_id' : fields.many2one(string='Confirmed By', obj='res.users', readonly=True),                       
+                                'approved_time' : fields.datetime(string='Approved Time', readonly=True),
+                                'approved_user_id' : fields.many2one(string='Approved By', obj='res.users', readonly=True),     
+                                'processed_time' : fields.datetime(string='Processed Time', readonly=True),
+                                'processed_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),             
+                                'cancelled_time' : fields.datetime(string='Processed Time', readonly=True),
+                                'cancelled_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),                                                                                             
+                                'cancelled_reason' : fields.text(string='Cancelled Reason', readonly=True),
+                                }   
+                
+    _defaults = {
+                            'name' : default_name,
+                            'state' : default_state,
+                            'created_time' : default_created_time,
+                            'created_user_id' : default_created_user_id,
+                            }
 
-		
-		
+    def workflow_action_confirm(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.create_sequence(cr, uid, id):
+                return False
+
+            if not self.log_audit_tral(cr, uid, id, 'confirmed'):
+                return False
+        return True
+
+    def workflow_action_approve(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.log_audit_trail(cr, uid, id, 'approved'):
+                return False
+        return True         
+        
+    def workflow_action_done(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.log_audit_trail(cr, uid, id, 'processed'):
+                return False
+        return True     
+        
+    def workflow_action_cancel(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.log_audit_trail(cr, uid, id, 'cancelled'):
+                return False
+        return True     
+        
+        
+    def button_action_set_to_draft(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.delete_workflow_instance(self, cr, uid, id):
+                return False
+
+            if not self.create_workflow_instance(self, cr, uid, id):
+                return False
+                
+        return True
+
+        
+    def button_action_cancel(self, cr, uid, ids, context={}):
+        wkf_service = netsvc.LocalService('workflow')
+        for id in ids:
+            if not self.delete_workflow_instance(self, cr, uid, id):
+                return False
+
+            if not self.create_workflow_instance(self, cr, uid, id):
+                return False
+
+            wkf_service.trg_validate(uid, 'pajak.formulir_1111_b2', id, 'button_cancel', cr)
+
+        return True
+
+    def log_audit_trail(self, cr, uid, id, event):
+        #TODO: Ticket #62
+        return True
+
+    def delete_workflow_instance(self, cr, uid, id):
+        #TODO: Ticket #63
+        return True
+
+    def create_workflow_instance(self, cr, uid, id):
+        #TODO: Ticket #64
+        return True
+            
+    def onchange_company_id(self, cr, uid, ids, comapny_id):
+        #TODO: Ticket #65
+        value = {}
+        domain = {}
+        warning = {}
+        
+        return {'value' : value, 'domain' : domain, 'warning' : warning}                   
+
+    def create_sequence(cr, uid, id):
+        #TODO: Ticket #66
+        return True
+
+        
+        
 
 formulir_1111_b2()
 
--- object_module/formulir_1111_b3.py
+++ object_module/formulir_1111_b3.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution   
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -28,125 +28,146 @@ from openerp import pooler
 from datetime import datetime
 
 class formulir_1111_b3(osv.osv):
-	_name = 'pajak.formulir_1111_b3'
-	_description = 'Formulir  1111 B3'
-	_inherit = ['mail.thread']
-	
-	def default_state(self, cr, uid, context={}):
-		return 'draft'
-		
-	def default_name(self, cr, uid, context={}):
-		return '/'
-		
-	def default_created_time(self, cr, uid, context={}):
-		#TODO: Ticket #5
-		return False
-		
-	def default_created_user_id(self, cr, uid, context={}):
-		#TODO: Ticket #6
-		return False
+    _name = 'pajak.formulir_1111_b3'
+    _description = 'Formulir  1111 B3'
+    _inherit = ['mail.thread']
+    
+    def default_state(self, cr, uid, context={}):
+        return 'draft'
+        
+    def default_name(self, cr, uid, context={}):
+        return '/'
+        
+    def default_created_time(self, cr, uid, context={}):
+        #TODO: Ticket #71
+        return False
+        
+    def default_created_user_id(self, cr, uid, context={}):
+       return uid
 
-	
-			
-	
-	_columns = 	{
-								'name' : fields.char(string='# SPT', size=30, required=True, readonly=True),
-								'company_id' : fields.many2one(string='Perusahaan', obj='res.company', required=True),
-								'nama_pkp' : fields.char(string='Nama PKP', size=255, required=True),
-								'npwp' : fields.char(string='NPWP', size=50, required=True),
-								'masa_pajak_id' : fields.many2one(string='Masa Pajak', obj='pajak.masa_pajak', required=True),
-								'pembetulan_ke' : fields.integer(string='Pembetulan Ke-', required=True),
-								'detail_ids' : fields.one2many(string='Detail', obj='pajak.detail_formulir_1111_b3', fields_id='formulir_id'),
-								'note' : fields.text(string='Note'),                            
-								'state' : fields.selection([('draft','Draft'),('confirm','Waiting For Approval'),('approve','Ready To Process'),('done','Done'),('cancel','Cancel')], 'Status', readonly=True),
-								'created_time' : fields.datetime(string='Created Time', readonly=True),
-								'created_user_id' : fields.many2one(string='Created By', obj='res.users', readonly=True),
-								'confirmed_time' : fields.datetime(string='Confirmed Time', readonly=True),
-								'confirmed_user_id' : fields.many2one(string='Confirmed By', obj='res.users', readonly=True),						
-								'approved_time' : fields.datetime(string='Approved Time', readonly=True),
-								'approved_user_id' : fields.many2one(string='Approved By', obj='res.users', readonly=True),		
-								'processed_time' : fields.datetime(string='Processed Time', readonly=True),
-								'processed_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),				
-								'cancelled_time' : fields.datetime(string='Processed Time', readonly=True),
-								'cancelled_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),																								
-								'cancelled_reason' : fields.text(string='Cancelled Reason', readonly=True),
-								}	
-				
-	_defaults =	{
-							'name' : default_name,
-							'state' : default_state,
-							'created_time' : default_created_time,
-							'created_user_id' : default_created_user_id,
-							}
-
-	def workflow_action_confirm(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'confirm'})
-		return True
-
-	def workflow_action_approve(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'approve'})
-		return True			
-		
-	def workflow_action_done(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'done'})
-		return True		
-		
-	def workflow_action_cancel(self, cr, uid, ids, context={}):
-		for id in ids:
-			self.write(cr, uid, [id], {'state' : 'cancel'})
-		return True		
-		
-
-
-		
-	def button_action_set_to_draft(self, cr, uid, ids, context={}):
-		for id in ids:
-			if not self.delete_workflow_instance(self, cr, uid, id):
-				return False
-
-			if not self.create_workflow_instance(self, cr, uid, id):
-				return False
-				
-		return True
-
-		
-        def button_action_cancel(self, cr, uid, ids, context={}):
-                wkf_service = netsvc.LocalService('workflow')
-                for id in ids:
-                                if not self.delete_workflow_instance(self, cr, uid, id):
-                                        return False
-
-                                if not self.create_workflow_instance(self, cr, uid, id):
-                                        return False
-
-                                wkf_service.trg_validate(uid, 'pajak.formulir_1111_b3', id, 'button_cancel', cr)
-
-                return True
-
-        def log_audit_trail(self, cr, uid, id, event):
-                #TODO: Ticket #12
-                return True
-
-        def delete_workflow_instance(self, cr, uid, id):
-                #TODO: Ticket #13
-                return True
-
-        def create_workflow_instance(self, cr, uid, id):
-                #TODO: Ticket #14
-                return True
+    def function_amount_all(self, cr, uid, ids, name, args, context=None):
+        #TODO: Ticket #72
+        res = {}
+        for id in ids:
+            res[id] =    {
+                        'total_dpp' : 0.0,
+                        'total_ppn' : 0.0,
+                        'total_ppnbm' : 0.0,
+                        }
+        return res
+    
+    _columns =  {
+                                'name' : fields.char(string='# SPT', size=30, required=True, readonly=True),
+                                'company_id' : fields.many2one(string='Perusahaan', obj='res.company', required=True), #TODO: Ticet #73
+                                'nama_pkp' : fields.char(string='Nama PKP', size=255, required=True), #TODO: Ticket #73
+                                'npwp' : fields.char(string='NPWP', size=50, required=True), #TODO: Ticket #73
+                                'masa_pajak_id' : fields.many2one(string='Masa Pajak', obj='pajak.masa_pajak', required=True), #TODO: Ticket #73
+                                'pembetulan_ke' : fields.integer(string='Pembetulan Ke-', required=True), #TODO: Ticket #73
+                                'detail_ids' : fields.one2many(string='Detail', obj='pajak.detail_formulir_1111_b3', fields_id='formulir_id'), #TODO: Ticket #73
+                                'total_dpp' : fields.function(string='Total DPP', fnct=function_amount_all, digits_compute=dp.get_precision('Account'), method=True, store=True, multi='all'),
+                                'total_ppn' : fields.function(string='Total PPN', fnct=function_amount_all, digits_compute=dp.get_precision('Account'), method=True, store=True, multi='all'),
+                                'total_ppnbm' : fields.function(string='Total PPnBM', fnct=function_amount_all, digits_compute=dp.get_precision('Account'), method=True, store=True, multi='all'),
+                                'note' : fields.text(string='Note'),                            
+                                'state' : fields.selection([('draft','Draft'),('confirm','Waiting For Approval'),('approve','Ready To Process'),('done','Done'),('cancel','Cancel')], 'Status', readonly=True),
+                                'created_time' : fields.datetime(string='Created Time', readonly=True),
+                                'created_user_id' : fields.many2one(string='Created By', obj='res.users', readonly=True),
+                                'confirmed_time' : fields.datetime(string='Confirmed Time', readonly=True),
+                                'confirmed_user_id' : fields.many2one(string='Confirmed By', obj='res.users', readonly=True),                       
+                                'approved_time' : fields.datetime(string='Approved Time', readonly=True),
+                                'approved_user_id' : fields.many2one(string='Approved By', obj='res.users', readonly=True),     
+                                'processed_time' : fields.datetime(string='Processed Time', readonly=True),
+                                'processed_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),             
+                                'cancelled_time' : fields.datetime(string='Processed Time', readonly=True),
+                                'cancelled_user_id' : fields.many2one(string='Process By', obj='res.users', readonly=True),                                                                                             
+                                'cancelled_reason' : fields.text(string='Cancelled Reason', readonly=True),
+                                }   
                 
-        def onchange_company_id(self, cr, uid, ids, comapny_id):
-            value = {}
-            domain = {}
-            warning = {}
-            
-            return {'value' : value, 'domain' : domain, 'warning' : warning}                  
+    _defaults = {
+                            'name' : default_name,
+                            'state' : default_state,
+                            'created_time' : default_created_time,
+                            'created_user_id' : default_created_user_id,
+                            }
 
-		
-		
+    def workflow_action_confirm(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.create_sequence(cr, uid, id):
+                return False
+
+            if not self.log_audit_trail(cr, uid, id, 'confirmed'):
+                return False
+
+        return True
+
+    def workflow_action_approve(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.log_audit_trail(cr, uid, id, 'approved'):
+                return False
+
+        return True         
+        
+    def workflow_action_done(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.log_audit_trail(cr, uid, id, 'processed'):
+                return False
+
+        return True     
+        
+    def workflow_action_cancel(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.log_audit_trail(cr, uid, id, 'cancelled'):
+                return False
+
+        return True     
+        
+        
+    def button_action_set_to_draft(self, cr, uid, ids, context={}):
+        for id in ids:
+            if not self.delete_workflow_instance(self, cr, uid, id):
+                return False
+
+            if not self.create_workflow_instance(self, cr, uid, id):
+                return False
+                
+        return True
+
+        
+    def button_action_cancel(self, cr, uid, ids, context={}):
+        wkf_service = netsvc.LocalService('workflow')
+        for id in ids:
+            if not self.delete_workflow_instance(self, cr, uid, id):
+                return False
+
+            if not self.create_workflow_instance(self, cr, uid, id):
+                return False
+
+            wkf_service.trg_validate(uid, 'pajak.formulir_1111_b3', id, 'button_cancel', cr)
+
+        return True
+
+    def log_audit_trail(self, cr, uid, id, event):
+        #TODO: Ticket #74
+        return True
+
+    def delete_workflow_instance(self, cr, uid, id):
+        #TODO: Ticket #75
+        return True
+
+    def create_workflow_instance(self, cr, uid, id):
+        #TODO: Ticket #76
+        return True
+            
+    def onchange_company_id(self, cr, uid, ids, comapny_id):
+        #TODO: Ticket #77
+        value = {}
+        domain = {}
+        warning = {}
+        
+        return {'value' : value, 'domain' : domain, 'warning' : warning}                  
+
+    def create_instance(self, cr, uid, id):
+        #TODO: Ticket #78
+        return True
 
 formulir_1111_b3()
 
