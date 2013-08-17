728c3f78f5484fb54d596151a9bed7c7ff391ac2
mike <viriyananda.michael@gmail.com>
2013-08-17 17:25:46 +0700

    fixed #5

--- object_module/faktur_pajak.py
+++ object_module/faktur_pajak.py
@@ -48,8 +48,7 @@ class faktur_pajak(osv.osv):
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
     def default_created_time(self, cr, uid, context={}):
-        #TODO: Ticket #5
-        return False
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
     def default_created_user_id(self, cr, uid, context={}):
         #TODO: Ticket #6
