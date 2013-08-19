089005cbb247c93d8a012be1b2130d9a6ebf56c6
mike <mike@mike.(none)>
2013-08-19 15:24:26 +0700

    fixed #63

--- object_module/formulir_1111_b2.py
+++ object_module/formulir_1111_b2.py
@@ -170,6 +170,10 @@ class formulir_1111_b2(osv.osv):
 
     def delete_workflow_instance(self, cr, uid, id):
         #TODO: Ticket #63
+
+        wkf_service = netsvc.LocalService('workflow')
+        wkf_service.trg_delete(uid, 'pajak.formulir_1111_b2', id, cr)
+
         return True
 
     def create_workflow_instance(self, cr, uid, id):
