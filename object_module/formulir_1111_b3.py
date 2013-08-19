939f71d1c585677a15b11c3d02fb9b35b1644e47
mike <mike@mike.(none)>
2013-08-19 15:27:43 +0700

    fixed #76

--- object_module/formulir_1111_b3.py
+++ object_module/formulir_1111_b3.py
@@ -178,6 +178,10 @@ class formulir_1111_b3(osv.osv):
 
     def create_workflow_instance(self, cr, uid, id):
         #TODO: Ticket #76
+
+        wkf_service = netsvc.LocalService('workflow')
+        wkf_service.trg_create(uid, 'pajak.formulir_1111_b3', id, cr)
+
         return True
             
     def onchange_company_id(self, cr, uid, ids, company_id):
