bb530562a8a0a2101bfdea97ffe9ad79fe2be9ab
mike <mike@mike.(none)>
2013-08-23 00:19:32 +0700

    fixed #69

--- object_module/formulir_1111_b1.py
+++ object_module/formulir_1111_b1.py
@@ -49,11 +49,11 @@ class formulir_1111_b1(osv.osv):
     def function_amount_all(self, cr, uid, ids, name, arg, context=None):
         #TODO: Ticket #69
         res = {}
-        total_dpp = 0.0
-        total_ppn = 0.0
-        total_ppnbm = 0.0
         
         for formulir in self.browse(cr, uid, ids):
+            total_dpp = 0.0
+            total_ppn = 0.0
+            total_ppnbm = 0.0
             if formulir.detail_ids:
                 for detail in formulir.detail_ids:
                     total_dpp += detail.dpp
