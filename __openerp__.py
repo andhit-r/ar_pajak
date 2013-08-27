06efe99d2f2d70ab70b503df5d1dac638bff34f6
Andhitia Rama <andhitia.r@gmail.com>
2013-08-27 17:06:11 +0700

    Gatekeeper revision :
    1. Penambahan dependensi ke report_aeroo
    2. Penambahan laporan untuk Surat Setor Pajak (pajak.surat_setor_pajak)

--- __openerp__.py
+++ __openerp__.py
@@ -51,6 +51,7 @@ Accounting connection will be handle on additional module.
                     'data/data_StatusFakturPajak.xml',
                     'data/pajak.akun_pajak.csv',
                     'data/pajak.jenis_setor_pajak.csv',
+                    'ar_pajak_report.xml',
                     'workflow/workflow_FakturPajak.xml',
                     'workflow/workflow_SuratSetorPajak.xml',
                     'workflow/workflow_Formulir1111.xml',
